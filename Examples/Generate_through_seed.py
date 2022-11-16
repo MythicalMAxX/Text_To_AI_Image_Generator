e341c2678e284a8fed1a09e31ba6fbfa2d0556da
Vinamra Yadav <73683711+MythicalMAxX@users.noreply.github.com>
2022-11-16 22:48:57 +0530

    Create Generate_through_seed.py

---
+++ Examples/Generate_through_seed.py
@@ -0,0 +1,45 @@
+from huggingface_hub import notebook_login
+
+notebook_login()
+
+import torch
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16)
+
+pipe = pipe.to("cuda")
+
+import torch
+
+
+def Generate_through_seed(Prompt,Seed_number):
+  generator = torch.Generator("cuda").manual_seed(Seed_number)
+  image = pipe(Prompt, generator=generator).images[0]
+  return image
+
+
+def prompt():
+  try:
+    prompt = input("Enter your Prompt:")
+    if prompt == "":
+      print("Prompt can't be empty")
+      exit
+    else:
+      return prompt
+  except:
+    print("An unidentical Error occoured")
+    exit()
+
+def SeedInput():
+  try:
+    seed = int(input("Enter a Seed:"))
+  except ValueError:
+      print("Try using Integer next time")
+      exit()
+  return seed
+
+
+Prompt = prompt()
+Seed_number = SeedInput()
+image = Generate_through_seed(Prompt,Seed_number)
+image.save(f"image.png")
